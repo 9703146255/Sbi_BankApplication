fbab17ed80209d8f3789a99134ca62e7df9a489e
thiruit <thiruit@gmail.com>
2023-12-23 22:23:37 +0530

    orrevert "Update Employee.java"
    This reverts commit 1e9a711b379f2e4cc3a5089ec5b4a66fcc5f7b44.

--- src/main/java/in/thiru/entity/Employee.java
+++ src/main/java/in/thiru/entity/Employee.java
@@ -12,11 +12,8 @@ public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="emp_id")
 	private Integer empId;
-	@Column(name="emp_name")
 	private String empName;
-	@Column(name="emp_sal")
 	private Double empSal;
 
 	public Integer getEmpId() {
